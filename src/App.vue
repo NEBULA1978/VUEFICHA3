2c47dc4cbae1f46284e485273b8539c835748ba9
NEBULA1978 <rpascualey@gmail.com>
2023-08-31 11:28:12 +0200

    comunicamos el padre con el hijo con props

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
-
+<!-- comunicamos el padre con el hijo con props -->
 <template>
-  <h1>Padre</h1>
-  <FichaVue/>
+  <!-- <h1>Padre</h1> -->
+  <div>
+    <div v-for="(cosa,index) in cosas" :key="index">
+      <FichaVue :cosa="cosa" :index="index"/>
+    </div>
+  </div>
+
+    
 </template>
   
 <script setup>
   // import HelloWorld from './components/HelloWorld.vue'
   import FichaVue from './components/Ficha.vue'
+    import cosas from "./assets/datos"
 </script>
 
 <style scoped></style>
--- src/components/Ficha.vue
+++ src/components/Ficha.vue
@@ -1,10 +1,24 @@
 
 <template>
-    <h1>Hijo</h1>
+    <!-- <h1>Hijo</h1> -->
+    <div>
+        <button class="boton">
+            {{ cosa.ropa }} ({{ cosa.marca }})
+        </button>
+        <div class="estrellas">
+            <span v-for="index in cosa.nota" :key="index">
+                <img src="https://www.html6.es/img/estrella.png" alt="">
+            </span>
+        </div>
+        <button>+</button>
+    </div>
 </template>
 
 <script setup>
-
+const props = defineProps({
+    cosa: String,
+    index: Number
+})
 </script>
 
 <style scoped></style>
